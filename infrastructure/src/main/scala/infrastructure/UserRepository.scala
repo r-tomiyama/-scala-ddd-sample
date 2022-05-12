884fb23cfeb3c4672c3991ddfa219b24beffd19c
r-tomiyama <r.tomiyama@opt.ne.jp>
2022-05-12 21:24:34 +0900

    fix: commnet

--- infrastructure/src/main/scala/infrastructure/UserRepository.scala
+++ infrastructure/src/main/scala/infrastructure/UserRepository.scala
@@ -5,27 +5,25 @@ import domainModel.user.{User, UserId, UserName, IUserRepository}
 import scala.util.Try
 
 class UserRepositoryImpl extends IUserRepository {
+  // TODO: 永続化サービスアクセスする（ORMを利用するのでDAOクラスは用意するが、入出力の型は変えない想定）
   // TODO: ID採番方法をDB依存に変える（ファクトリ）
   // TODO: トランザクションを取り入れる
+
   def find(userName: UserName): Option[User] =
-    // 永続化サービスアクセス
     if (userName.value == "すでにある名前")
       Some(User("1", userName.value))
     else
       None
 
   def find(userId: UserId): Option[User] =
-    // 永続化サービスアクセス
     if (userId.value == "1")
       Some(User("1", "すでにある名前"))
     else
       None
 
   def save(user: User): Try[User] =
-    // 永続化サービスアクセス
     Try(user)
 
   def delete(user: User): Try[Unit] =
-    // 永続化サービスアクセス
     Try(Unit)
 }
